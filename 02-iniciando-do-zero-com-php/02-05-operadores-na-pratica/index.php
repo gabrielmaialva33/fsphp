79ef8928c9cfda06b062c9e45dee579feb0b75be
Gabriel Maia <gabrielmaialva33@gmail.com>
2020-10-20 00:28:37 -0300

    operadores

--- 02-iniciando-do-zero-com-php/02-05-operadores-na-pratica/index.php
+++ 02-iniciando-do-zero-com-php/02-05-operadores-na-pratica/index.php
@@ -8,20 +8,45 @@ echo fullStackPHPClassName("02.05 - Operadores na prática");
  */
 fullStackPHPClassSession("atribuição", __LINE__);
 
+$operatorA = 5;
+$operators = [
+    "a += 5" => ($operatorA += 5),
+    "a -= 5" => ($operatorA -= 5),
+    "a *= 5" => ($operatorA *= 5),
+    "a /= 5" => ($operatorA /= 5),
+];
+
+var_dump($operators);
+
 
 /**
  * [ comparação ] https://php.net/manual/pt_BR/language.operators.comparison.php
  */
 fullStackPHPClassSession("comparação", __LINE__);
 
+$relatedA = 5;
+$relatedB = "5";
+$relatedC = 10;
+
+var_dump($relatedA || $relatedB);
 
 /**
  * [ lógicos ] https://php.net/manual/pt_BR/language.operators.logical.php
  */
 fullStackPHPClassSession("lógicos", __LINE__);
 
+$logicA = false;
+$logicB = true;
+
+var_dump($logicA || $logicB);
 
 /**
  * [ aritiméticos ] https://php.net/manual/pt_BR/language.operators.arithmetic.php
  */
 fullStackPHPClassSession("aritiméticos", __LINE__);
+
+$A = 5;
+$B = 10;
+$C = $A + $B;
+
+var_dump($C);
