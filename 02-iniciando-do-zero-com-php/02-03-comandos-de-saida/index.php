63c892af83d8ac58d4a16b918efdfb66dda73c73
Gabriel Maia <gabrielmaialva@gmail.com>
2020-10-20 00:15:40 -0300

    comandos de saida

--- 02-iniciando-do-zero-com-php/02-03-comandos-de-saida/index.php
+++ 02-iniciando-do-zero-com-php/02-03-comandos-de-saida/index.php
@@ -25,27 +25,55 @@ echo "{$fsphp}"; // -> proteção de variável
  */
 fullStackPHPClassSession("print", __LINE__);
 
-print
+$php_print = "<h2> O print também pode receber um HTML :O </h2>";
+
+print $fsphp;
+print $php_print;
+
+print "<h3>{$fsphp} {$php_print}</h3>";
 
 /**
  * [ print_r ] https://php.net/manual/pt_BR/function.print-r.php
  */
 fullStackPHPClassSession("print_r", __LINE__);
 
+$arrey = ["PHP" => "😩", "😡" => "😨"];
+
+print_r($arrey);
+
+echo "<pre>", print_r($arrey, true), "</pre>";
 
 /**
  * [ printf ] https://php.net/manual/pt_BR/function.printf.php
  */
 fullStackPHPClassSession("printf", __LINE__);
 
+$article = "<article><h1>%s</h1><p>%s</p></article>";
+$title = "Article";
+$subtitle = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to 
+make a type specimen book. It has survived not only five centuries, but also the leap into electronic 
+typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
+including versions of Lorem Ipsum.";
+
+printf($article, $title, $subtitle);
+echo sprintf($article, $title, $subtitle);
 
 /**
  * [ vprintf ] https://php.net/manual/pt_BR/function.vprintf.php
  */
 fullStackPHPClassSession("vprintf", __LINE__);
 
+$text = "<article><h1>Php %s</h1><p>😡 %s</p></article>";
+$arrey_v = ["PHP" => "😩", "😡" => "😨"];
+
+vprintf($text,$arrey_v);
+
 
 /**
  * [ var_dump ] https://php.net/manual/pt_BR/function.var-dump.php
  */
 fullStackPHPClassSession("var_dump", __LINE__);
+
+var_dump($arrey, $fsphp, $short);
--- 02-iniciando-do-zero-com-php/02-04-variaves-e-tipos-de-dados/index.php
+++ 02-iniciando-do-zero-com-php/02-04-variaves-e-tipos-de-dados/index.php
@@ -8,20 +8,62 @@ fullStackPHPClassName("02.04 - Variáveis e tipos de dados");
  */
 fullStackPHPClassSession("variáveis", __LINE__);
 
+$userFistName = "Gabriel";
+$userLastName = "Maia";
+
+echo "<h3>{$userFistName} {$userLastName}</h3>";
+
+$user_fist_name = $userFistName;
+$user_last_name = $userLastName;
+
+$userAge = 21;
+
+echo "<h3>{$user_fist_name} {$user_last_name} <span class='tag'>{$userAge}</h3>";
+
+$userSchool = "FATEC";
+$$userSchool = "GCM";
+
+echo "<h3>{$userSchool} {$FATEC}</h3>";
+
+$calcA = 10;
+$calcB = 20;
+
+var_dump([
+    "a" => $calcA,
+    "b" => $calcB
+]);
 
 /**
  * [ tipo boleano ] true | false
  */
 fullStackPHPClassSession("tipo boleano", __LINE__);
 
+$true = true;
+$false = false;
+
+var_dump($true, $false);
+
+var_dump($userAge < 10);
 
 /**
  * [ tipo callback ] call | closure
  */
 fullStackPHPClassSession("tipo callback", __LINE__);
 
+$code = "<article><h1>Um Call Uer Function!</h1></article>";
+$codeClear = call_user_func("strip_tags", $code);
+
+var_dump($code, $codeClear);
 
 /**
  * [ outros tipos ] string | array | objeto | numérico | null
  */
 fullStackPHPClassSession("outros tipos", __LINE__);
+
+
+$string = "Olá mundo";
+$array = [$string];
+$object = new StdClass();
+$object -> hello = $string;
+
+var_dump($string, $array, $object);
